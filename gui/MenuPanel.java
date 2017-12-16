3a888ee8738b52dbe1a5854ef44ddc02c72bf722
dsipahioglu <32214865+dsipahioglu@users.noreply.github.com>
2017-12-16 14:30:20 +0300

    Update MenuPanel.java

--- gui/MenuPanel.java
+++ gui/MenuPanel.java
@@ -1,55 +1,48 @@
 /**
-* @author  Deniz Sipahioğlu
+* @author  Deniz Sipahioglu
 * @version 1.0
 * Created: 10/20/2017 
 */
-package gui;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.GridLayout;
 import java.awt.CardLayout;
 import java.awt.BorderLayout;
-import java.util.ArrayList;
+import java.util.ArrayList; 
 import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
 
 public class MenuPanel extends JPanel{
- 
- ArrayList<MenuButton> menuButtons;
  Image image;
+ ArrayList<MenuButton> menuButtons;
  
  public MenuPanel(){
   menuButtons = new ArrayList<>();
   setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
   setPreferredSize(new Dimension(600, 600));
   try{
-    image = ImageIO.read(new File("../Images/bg.png"));
-  }
-  catch(IOException ex){
-    System.out.println("BG img not found.");
-  }
-  add(Box.createRigidArea(new Dimension(0,120)));
-  addButton("Play", PanelType.PLAY);
-  addButton("Settings", PanelType.SETTINGS);
+  image = ImageIO.read(new File("../Images/bg.png"));
+  }catch(IOException ex){System.out.println("not found");}
+  add(Box.createRigidArea(new Dimension(0, 120)));
+  addButton(PanelType.PLAY);
+  addButton(PanelType.SETTINGS);
+  addButton(PanelType.EXIT);
  }
  
  public ArrayList<MenuButton> getButtons(){
   return menuButtons;
  }
  
- private void addButton(String text, PanelType panelType){
+ private void addButton(PanelType panelType){
   add(Box.createRigidArea(new Dimension(0, 25)));
-  
-  MenuButton button = new MenuButton(text, panelType);
+  MenuButton button = new MenuButton(panelType);
   button.setAlignmentX(Component.CENTER_ALIGNMENT);
   menuButtons.add(button);
   add(button);
  }
- 
  public void paintComponent(Graphics g)
  {
-   g.drawImage(image, 0, 0, null);
+     g.drawImage(image, 0, 0, null);
  }
 }
